107dacdc29a8d1bfa364bc6b05d1ca3380d82a17
Karolina Wysocka <karolinawyso@mat.umk.pl>
2024-11-30 16:05:19 +0100

    Dodanie funkcji silni do menu

--- projekt1.cpp
+++ projekt1.cpp
@@ -16,8 +16,16 @@ int main() {
         cout << endl;
         cout << "MENU" << endl;
         cout << "Podaj numer czynnosci, ktora chcesz wykonac" << endl;
+	cout << "1. Oblicz silnie" << endl;
         cout << "0. Wyjscie" << endl;
         cin >> wyjscie;
+
+	if(wyjscie==1){
+		int n;
+		cout << "Podaj n: ";
+		cin >> n;
+		cout << silnia(n) << endl;
+	}
     } while(wyjscie != 0);
     return 0;
 }
